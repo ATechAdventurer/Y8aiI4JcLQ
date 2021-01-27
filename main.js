09603a2ed07a11a41264072adcd12dc6a847041f
Cameron Steele <cam.steeleis@gmail.com>
2021-01-27 11:17:11 -0600

    Added MVP functionality

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 
 const rules = require('./rules.json');
-const {NO_COMPATIBLE_SERVERS_FOUND} = require('./lib/constants');
+const { NO_COMPATIBLE_SERVERS_FOUND } = require('./lib/constants');
+const { buildEmbed } = require('./lib/embed');
 const bot = new Client();
 
 bot.on('ready', _ => {
     console.log(`Logging in with identity: ${bot.user.tag}`);
     const guilds = bot.guilds.cache.array();
     const compatibleGuilds = guilds.filter(guild => rules.hasOwnProperty(guild.id));
-    if(compatibleGuilds.length <= 0){
+    if (compatibleGuilds.length <= 0) {
         throw new Error(NO_COMPATIBLE_SERVERS_FOUND);
     }
 })
 
-bot.login("");
+bot.on('message', (msg) => {
+    if (msg.author.bot || !msg.content.startsWith(process.env.DISCRIMINATOR)) {
+        return;
+    }
+    const formattedCommand = msg.content.replace(process.env.DISCRIMINATOR, "").split(" ");
+    switch (formattedCommand[0]) {
+        case "rules":
+            msg.channel.send(buildEmbed(msg.guild.id));
+            break;
+    }
+})
+
+bot.login(process.env.DISCORD_BOT_TOKEN);
